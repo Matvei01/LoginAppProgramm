16fe359d6ed21e0330087da82168644dc0b91bc5
Matvei <khlestov.matvei@mail.ru>
2024-03-30 20:20:27 +0500

    Created base project, removed storyboard references

---
+++ LoginApp/LoginApp/App/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  LoginApp
+//
+//  Created by Matvei Khlestov on 30.03.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+    
+    // MARK: UISceneSession Lifecycle
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
+
---
+++ LoginApp/LoginApp/Controllers/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  LoginApp
+//
+//  Created by Matvei Khlestov on 30.03.2024.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
